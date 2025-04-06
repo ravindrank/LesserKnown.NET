ee2d1c01781fd53cda2347672595ddf016255f73
Ravindran Keshavan <ravindran.k@gmail.com>
2025-04-06 21:26:23 +0530

    Add Init only properties

---
+++ 5_InitOnlyProps.cs
@@ -0,0 +1,13 @@
+﻿namespace LesserKnown.NET;
+
+public class Person
+{
+    public string Name { get; init; }
+
+    public void Run()
+    {
+        var person = new Person { Name = "John" };
+        //person.Name = "Doe"; // Error: Cannot modify init-only property
+    }
+
+}
