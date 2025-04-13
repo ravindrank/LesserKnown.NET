f9df0d1daaa7c8efe56723ac03d2cf81a9cdf183
Ravindran Keshavan <ravindran.k@gmail.com>
2025-04-13 12:12:10 +0530

    Add MessagePack Demo

--- 0_Program.cs
+++ 0_Program.cs
@@ -1,4 +1,6 @@
-﻿namespace LesserKnown.NET;
+﻿using MessagePack;
+
+namespace LesserKnown.NET;
 
 public class Program
 {
@@ -34,5 +36,36 @@ public class Program
         };
 
         Person p3 = new Person();
+
+
+        // Message Pack Demo
+
+        MessagePackBinaryDemo mc = new MessagePackBinaryDemo
+        {
+            Age = 50,
+            FirstName = "Bill",
+            LastName = "Yeates",
+        };
+
+        byte[] bytes = MessagePackSerializer.Serialize(mc);
+        MessagePackBinaryDemo mcd = MessagePackSerializer.Deserialize<MessagePackBinaryDemo>(bytes);
+
+        // You can dump MessagePack binary blobs to human readable json.
+        // Using indexed keys (as opposed to string keys) will serialize to MessagePack arrays,
+        // hence property names are not available.
+
+        var json = MessagePackSerializer.ConvertToJson(bytes);
+        Console.WriteLine(json);
+        
+        MessagePackJsonDemo mc2 = new MessagePackJsonDemo
+        {
+            Age = 40,
+            Name = "John",
+        };
+
+        byte[] bytes2 = MessagePackSerializer.Serialize(mc2);
+        MessagePackJsonDemo mcd2 = MessagePackSerializer.Deserialize<MessagePackJsonDemo>(bytes2);
+        var json2 = MessagePackSerializer.ConvertToJson(bytes2);
+        Console.WriteLine(json2);
     }
 }
